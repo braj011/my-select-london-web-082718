1db22f168df7beaa6b2fd6ddb99ca6341a6dd56a
braj011 <bhagyaraj01@gmail.com>
2018-08-21 20:26:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -9,14 +9,3 @@ end
 
 =begin 
 
-My_COLLECT - got help on this one
-def my_collect(words) # put argument(s) here
-  i = 0
-  new_array = []
-  while i < words.length
-    new_array << yield(words[i])
-    # words[i] is an argument to Yield, hence use ()
-    i += 1
-  end
-  new_array
-=end
