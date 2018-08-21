18e8af1a35f02e5ab399d90192edd0431b9362df
braj011 <bhagyaraj01@gmail.com>
2018-08-21 20:27:35 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   new_array = []
   while i < collection.length
-    new_array << yield(words[i])
+    new_array << yield(collection[i])
     i += 1
   end 
   new_array
